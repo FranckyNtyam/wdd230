e6b386c08d6e26a7b12491f5e204ea0991c078cf
Ntyam Adjomo Francky Ludovic <francky.ntyam@gmail.com>
2023-11-10 01:38:24 +0100

    Home page enhancement

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -4,3 +4,22 @@ let time = day.getHours() + ":" + day.getMinutes() + ":" + day.getSeconds();
 let date = day.getMonth() + 1 +"/" + day.getDate() + "/" + day.getFullYear();
 document.querySelector('#year').innerHTML = year;
 document.querySelector('#lastModified').innerHTML = "last modification: " + date + " " + time;
+
+// create hamburger button
+
+const hamburgerButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamburgerButton.addEventListener('click', () => {
+
+    navigation.classList.toggle('open');
+    hamburgerButton.classList.toggle('open');
+})
+
+const darkModeButton = document.querySelector('#dark-mode');
+const main = document.querySelector('main');
+
+darkModeButton.addEventListener('click', () => {
+    main.classList.toggle('dark-mode');
+    darkModeButton.classList.toggle('light-mode');
+});
