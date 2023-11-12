56401ada559f82ddb73428ad25d6453679aa1cd6
Ntyam Adjomo Francky Ludovic <francky.ntyam@gmail.com>
2023-11-12 02:40:01 +0100

    the chamber home page

---
+++ chamber/scripts/script.js
@@ -0,0 +1,18 @@
+
+// create hamburger button
+const hambergerButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hambergerButton.addEventListener('click', () => {
+
+    navigation.classList.toggle('open');
+    hambergerButton.classList.toggle('open');
+})
+
+// last modification 
+let day = new Date(document.lastModified);
+let year = day.getFullYear();
+let time = day.getHours() + ":" + day.getMinutes() + ":" + day.getSeconds();
+let date = day.getMonth() + 1 +"/" + day.getDate() + "/" + day.getFullYear();
+document.querySelector('#year').innerHTML = year;
+document.querySelector('#lastModified').innerHTML = "last modification: " + date + " " + time;
