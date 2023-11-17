66b7fb122098b157ced9b33a326e782b316a87ec
Ntyam Adjomo Francky Ludovic <francky.ntyam@gmail.com>
2023-11-17 14:55:08 +0100

    Home page - Responsive Images: apply number of visits

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -23,3 +23,27 @@ darkModeButton.addEventListener('click', () => {
     main.classList.toggle('dark-mode');
     darkModeButton.classList.toggle('light-mode');
 });
+// here is determine and display visits in our website.
+
+// initialize the variable displayVisitsNumber.
+const displayVisitsNumber = document.querySelector('.visits-site');
+
+// Get the stored value for the number of visits in local storage key.
+
+let numberOfVisits = Number(window.localStorage.getItem('numberOfVisitsLocalStorage')) || 0;
+
+// We determine if is the first visit or display the number of visits.
+
+if (numberOfVisits !== 0) {
+    displayVisitsNumber.textContent = numberOfVisits;
+}else {
+    displayVisitsNumber.textContent = `This is your first in our website. Welcome!`;
+}
+
+// increment number of visits by one.
+
+numberOfVisits++;
+
+// store the new visit total into localStorage key equal numberOfVisitsLocalStorage.
+
+localStorage.setItem("numberOfVisitsLocalStorage", numberOfVisits);
