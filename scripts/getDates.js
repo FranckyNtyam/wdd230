e261f94f3d3ee3349e40a278da9f4ee006640770
Ntyam Adjomo Francky Ludovic <francky.ntyam@gmail.com>
2023-11-17 16:02:36 +0100

    Home page - Responsive Images

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -7,7 +7,7 @@ document.querySelector('#lastModified').innerHTML = "last modification: " + date
 
 // create hamburger button
 
-const hamburgerButton = document.querySelector('#button-menu');
+const hamburgerButton = document.querySelector('#buttonMenu');
 const navigation = document.querySelector('.navigation');
 
 hamburgerButton.addEventListener('click', () => {
@@ -16,7 +16,7 @@ hamburgerButton.addEventListener('click', () => {
     hamburgerButton.classList.toggle('open');
 })
 
-const darkModeButton = document.querySelector('#dark-mode-button');
+const darkModeButton = document.querySelector('#buttonDarkMode');
 const main = document.querySelector('main');
 
 darkModeButton.addEventListener('click', () => {
