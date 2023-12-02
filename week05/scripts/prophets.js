eba8bff23646c8038c361b82064953362c53acfa
Ntyam Adjomo Francky Ludovic <francky.ntyam@gmail.com>
2023-12-02 10:50:15 +0100

    ftch api display list of the latter-day prophets

--- week05/scripts/prophets.js
+++ week05/scripts/prophets.js
@@ -1,31 +1,39 @@
 const url ='https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
 
 const cards = document.querySelector('#cards');
-async function getProphetData() {
+
+async function getProphetData(url) {
     const response = await fetch(url);
     const data = await response.json();
     // console.table(data.prophets);
-    displayProphets(data.prophets);
+     displayProphets(data.prophets);
 }
-
-getProphetData();
 
 const displayProphets = (prophets) => {
     prophets.forEach((prophet) => {
     let card = document.createElement('section');
-    let fullName = document.createElement('__');
+    let fullName = document.createElement('h2');
     let portrait = document.createElement('img');
+    let birthdate = document.createElement('p');
+    let placebirth = document.createElement('p');
 
-    fullName.textContent = ` $ {prophet._____} __________`;
+    fullName.textContent = ` ${prophet.name} ${prophet.lastname}`;
+    birthdate.textContent = `Date of Birth: ${prophet.birthdate}`;
+    placebirth.textContent = `Place of Birth: ${prophet.birthplace}`;
     portrait.setAttribute('src', prophet.imageurl);
-    portrait.setAttribute('alt', `Portrait of ${prophet._____} _______________`);
+    portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
     portrait.setAttribute('loading', 'lazy');
     portrait.setAttribute('width', '340');
     portrait.setAttribute('height', '440');
 
-    card.appendChild(______);
+    card.appendChild(fullName);
+    card.appendChild(birthdate);
+    card.appendChild(placebirth);
     card.appendChild(portrait);
 
     cards.appendChild(card);
+    // card.style.backgroundColor = '#658ddd'
     });
 }
+
+getProphetData(url);
