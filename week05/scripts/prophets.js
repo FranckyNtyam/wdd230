ff55d3107a3daeed9969c729f02db19e84456900
Ntyam Adjomo Francky Ludovic <francky.ntyam@gmail.com>
2023-12-02 11:33:06 +0100

    ftch api display list of the latter-day prophets

--- week05/scripts/prophets.js
+++ week05/scripts/prophets.js
@@ -23,8 +23,8 @@ const displayProphets = (prophets) => {
     portrait.setAttribute('src', prophet.imageurl);
     portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
     portrait.setAttribute('loading', 'lazy');
-    portrait.setAttribute('width', '340');
-    portrait.setAttribute('height', '440');
+    portrait.setAttribute('width', '300');
+    portrait.setAttribute('height', '400');
 
     card.appendChild(fullName);
     card.appendChild(birthdate);
