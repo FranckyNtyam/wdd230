b9f2e4ff02681a95c278552931c1bf0e9ec06071
Ntyam Adjomo Francky Ludovic <francky.ntyam@gmail.com>
2023-12-03 00:43:53 +0100

    JSON and the Weather API

--- scripts/links.js
+++ scripts/links.js
@@ -1,6 +1,8 @@
 const baseURL = "https://franckyntyam.github.io/wdd230/";
 const linksUrl = "https://franckyntyam.github.io/wdd230/data/links.json";
 
+const listItem = document.querySelector('#listItem');
+
 async function linksData() {
     const response = await fetch(linksUrl);
     const data = await response.json();
@@ -11,14 +13,27 @@ async function linksData() {
 linksData();
 
 const getLinks = (weeks) => {
-    weeks.forEach(weekNum =>{
-        let difWeek = document.querySelector('#weeks');
-        let descActivity = document.querySelector('#desc-activity');
-        difWeek.innerHTML =`${weekNum.week}:`;
-        
-           
-        
-        
+    weeks.forEach(weekNum => {
+        let difWeek = document.createElement('li');
+
+
+        difWeek.innerHTML = `${weekNum.week}:`;
+        listItem.appendChild(difWeek);
+        weekNum.links.forEach(function (link) {
+            let hyperRef = document.createElement('a');
+            let span = document.createElement('span');
+            let linkhref = link.url;
+            span.innerHTML = `  ${link.title} |`;
+            hyperRef.setAttribute('href', linkhref);
+            difWeek.appendChild(hyperRef);
+            hyperRef.appendChild(span);
+        });
+
+
+
+
+
+
 
     })
 }
